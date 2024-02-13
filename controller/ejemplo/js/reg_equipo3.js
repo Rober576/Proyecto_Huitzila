6933229e32801a5369e481981b594ac18b197930
Jorge Luis <luissanodvalrod727@gmail.com>
2024-02-12 20:14:24 -0600

    Modificaciones y conexión en registro Equipo 3, desde view hasta controller

---
+++ controller/ejemplo/js/reg_equipo3.js
@@ -0,0 +1,28 @@
+//declara las variables globales
+var formulario = document.getElementById('Equipo3');
+var submitButton = document.getElementById("submitButton");
+
+console.log('hola mundo3');
+
+submitButton.addEventListener("click", function(event) {
+    // Detenemos la acción por defecto del botón
+    event.preventDefault();
+
+
+    var datos= new FormData(formulario);
+    fetch('../../controller/ejemplo/registro_equipo3.php', {
+        method: 'POST',
+        body: datos
+    })
+
+    .then(res => res.json())
+    .then(data => {
+        console.log(data);
+        if (data === 'exito') {
+            const form= document.getElementById('Equipo3');
+            form.reset();
+            alert("Registro exitoso");
+            
+        }
+    })
+})
--- controller/ejemplo/registro_equipo3.php
+++ controller/ejemplo/registro_equipo3.php
@@ -1,11 +1,11 @@
 <?php
 // Recoger los valores de los campos del formulario
 include_once('../../model/ejemplo/registros/registroE3.php');
-$c1 = $_POST["id1"];
-$c2 = $_POST["id2"];
-$c3 = $_POST["id3"];
-$c4 = $_POST["id4"];
-$c5 = $_POST["id5"];
+$c1 = $_POST["campo1"];
+$c2 = $_POST["campo2"];
+$c3 = $_POST["campo3"];
+$c4 = $_POST["campo4"];
+$c5 = $_POST["campo5"];
 
 //instanciar la clase y llamar la funcion para insertar
 $obj = new NuevosCampos();
