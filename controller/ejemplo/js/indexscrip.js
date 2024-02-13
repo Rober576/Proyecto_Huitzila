9de31fdab8fd4b7c1f641cfebe2917337f2f533c
Barbarus1300 <fernanglezmaga@gmail.com>
2024-02-12 22:16:47 -0600

    cambis de rutas de los formularios y las tablas

--- controller/ejemplo/js/indexscrip.js
+++ controller/ejemplo/js/indexscrip.js
@@ -1,9 +1,9 @@
     // Agrega un listener a cada enlace de registrar para cambiar el src del iframe
     for (let i = 1; i <= 5; i++) {
         document.getElementById(`registrar${i}`).addEventListener('click', function() {
-          document.getElementById('frame').src = `view/ejemplo/formulario${i}.html`;
+          document.getElementById('frame').src = `/view/ejemplo/formulario${i}.html`;
         })
         document.getElementById(`mostrar${i}`).addEventListener('click', function() {
-          document.getElementById('frame').src = `view/ejemplo/tabla${i}.html`;
+          document.getElementById('frame').src = `/view/ejemplo/tabla${i}.html`;
         });
       }
