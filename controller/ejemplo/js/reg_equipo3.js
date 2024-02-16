e8b35038478495949b3bc1b2d9ce1fe346e89d44
diegofregoso <imdiegofregoso@gmail.com>
2024-02-15 21:15:55 -0600

    Validación de correo y contraseña equipo 3

--- controller/ejemplo/js/reg_equipo3.js
+++ controller/ejemplo/js/reg_equipo3.js
@@ -1,35 +1,33 @@
-//declara las variables globales
-var formulario = document.getElementById('Equipo3');
-var submitButton = document.getElementById("submitButton");
-
-console.log('hola mundo3');
-
-formulario.addEventListener("submit", function(event) {
-    // Detenemos la acción por defecto del botón
+document.getElementById('Equipo3').addEventListener('submit', function (event) {
     event.preventDefault();
 
+    var correoValido = val_Correo();
+    var passwordValido = val_Password();
+    var idValido = val_ID();
+    var nombreValido = val_Nombre();
+    var claveValida = val_clave();
 
-    var datos= new FormData(formulario);
-    fetch('../../controller/ejemplo/registro_equipo3.php', {
-        method: 'POST',
-        body: datos
-    })
-
-    .then(res => res.json())
-    .then(data => {
-        console.log(data);
-        if (data === 'exito') {
-            alert("Registro exitoso");
-        }
-
-        //los datos no pasaron alguna validacion
-        else if(data === 'errores'){
-            alert("Registro fallido");
-        }
-
-        //algo salió mal
-        else {
-            alert(data);
-        }
-    })
-})
+    if (correoValido && passwordValido && idValido && nombreValido && claveValida) {
+        var datos = new FormData(this);
+        fetch('../../controller/ejemplo/registro_equipo3.php', {
+            method: 'POST',
+            body: datos
+        })
+        .then(res => res.json())
+        .then(data => {
+            console.log(data);
+            if (data === 'exito') {
+                const form = document.getElementById('Equipo3');
+                form.reset();
+                alert("Registro exitoso");
+            }
+        })
+        .catch(error => {
+            console.error('Error:', error);
+            alert("Error al procesar el registro");
+        });
+    } else {
+        console.log("Registro fallido. Por favor, revise los campos resaltados.");
+        alert("Registro fallido. Por favor, revise los campos resaltados.");
+    }
+});
