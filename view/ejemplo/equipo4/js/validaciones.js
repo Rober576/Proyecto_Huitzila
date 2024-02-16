81e052a11e7bfd3b829c15ec84bc104ea99ca302
oscar23w <63673374+oscar23w@users.noreply.github.com>
2024-02-15 19:44:12 -0600

    Los campos de telefono, CP y dirección del equipo 4 ya eliminan los caracteres cuando no son validos

--- view/ejemplo/equipo4/js/validaciones.js
+++ view/ejemplo/equipo4/js/validaciones.js
@@ -27,6 +27,14 @@ const expresion = {
 
 Equipo4.campo1.addEventListener('keyup', (e) => {
     let valorInput = e.target.value;
+
+    // Elimina todos los caracteres no numéricos
+    valorInput = valorInput.replace(/\D/g, '');
+
+    // Limita la longitud a 10 caracteres
+    valorInput = valorInput.slice(0, 10);
+
+    Equipo4.campo1.value = valorInput;
    
     if (!expresion.telefono.test(valorInput)) {
         Equipo4.campo1.style.border = "5px solid red"; 
@@ -40,6 +48,14 @@ Equipo4.campo1.addEventListener('keyup', (e) => {
 
 Equipo4.campo2.addEventListener('keyup', (e) => {
     let valorInput = e.target.value;
+
+    // Elimina todos los caracteres no numéricos
+    valorInput = valorInput.replace(/\D/g, '');
+
+    // Limita la longitud a 10 caracteres
+    valorInput = valorInput.slice(0, 5);
+
+    Equipo4.campo2.value = valorInput;
 
     
     if (!expresion.codigoPostal.test(valorInput)) {
@@ -55,12 +71,18 @@ expresion.codigoPostal = /^\d{5}$/;
 
 
 
-expresion.direccion = /^[a-zA-Z0-9\s.#]+$/;
+const expresionDireccion = /^[a-zA-Z0-9\s.#]+$/;
 
 Equipo4.campo3.addEventListener('input', (e) => {
     let valorInput = e.target.value;
 
-    if (!expresion.direccion.test(valorInput) || valorInput.trim() === '') {
+    // Si la entrada no coincide con la expresión regular, eliminar los caracteres no válidos
+    if (!expresionDireccion.test(valorInput)) {
+        e.target.value = valorInput.replace(/[^a-zA-Z0-9\s.#]/g, '');
+    }
+
+    // Verificar si la entrada es una cadena vacía o no cumple con la expresión regular
+    if (!expresionDireccion.test(e.target.value) || e.target.value.trim() === '') {
         Equipo4.campo3.style.border = "5px solid red"; 
         bandera3 = false; 
     } else {
@@ -68,6 +90,7 @@ Equipo4.campo3.addEventListener('input', (e) => {
         bandera3 = true; 
     }
 });
+
 
 
 expresion.apellido = /^[a-zA-Z\s]{1,20}$/;
