94bd2adb205e179b90bb17c4a3efc068d8c2ecc1
AramisHS <mikunaka123@gmail.com>
2024-02-19 11:20:45 -0600

    Agregue columna en php para acciones equipo2

--- controller/ejemplo/mostrar_equipo2.php
+++ controller/ejemplo/mostrar_equipo2.php
@@ -14,6 +14,7 @@ if (isset($_POST['consulta'])) {
 $salida = '';
 
 if ($resultado) {
+    
     $salida .= '
         <table border="1">
             <thead>
@@ -23,17 +24,21 @@ if ($resultado) {
                     <th>Campo 3</th>
                     <th>Campo 4</th>
                     <th>Campo 5</th>
+                    <th>Acciones</th>
                 </tr>
             </thead>
             <tbody>';
 
     foreach ($resultado as $fila) {
+        $id = $fila['id'];
         $salida .= '<tr>';
         $salida .= '<td>' . $fila["campo1"] . '</td>';
         $salida .= '<td>' . $fila["campo2"] . '</td>';
         $salida .= '<td>' . $fila["campo3"] . '</td>';
         $salida .= '<td>' . $fila["campo4"] . '</td>';
         $salida .= '<td>' . $fila["campo5"] . '</td>';
+        $salida .= '<td><a href="#" class="table_item__link eliminar-elemento" data-id="' . $id . '">Eliminar</a></td>'; // Aquí va el link para eliminar, por ejemplo: href="eliminar.php?id='.$fila['id'].'
+        
         $salida .= '</tr>';
     }
 
@@ -44,3 +49,5 @@ if ($resultado) {
 
 echo $salida;
 ?>
+
+<script src="../../controller/ejemplo/js/Eliminar_Equipo3.js"></script>
