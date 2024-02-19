f7e5c702abf0fce3022311508ee5eb57690c2a06
Sofia <almarazsofia29@gmail.com>
2024-02-19 12:12:36 -0600

    interfaz de editar

--- controller/ejemplo/mostrar_equipo2.php
+++ controller/ejemplo/mostrar_equipo2.php
@@ -42,7 +42,7 @@ if ($resultado) {
         $salida .= '<td>';
         $salida .= '<a href="#" class="table_item__link eliminar-elemento" data-id="' . $id . '">Eliminar</a>';
         $salida .= ' | ';
-        $salida .= '<a href="#" class="table_item__link editar-elemento" data-id="' . $id . '">Editar</a>';
+        $salida .= '<a href="/view\ejemplo\modificar_equipo2.html" class="table_item__link editar-elemento" data-id="' . $id . '">Editar</a>';
         $salida .= '</td>';
         
         $salida .= '</tr>';
