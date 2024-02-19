b30578fa5003bb29dc976c62776bf6bf56852d65
KaronteElPro <alanaceistopherrr1@outlook.com>
2024-02-19 12:13:36 -0600

    Creacion de archivos

---
+++ controller/ejemplo/js/Modificar_Equipo4.js
@@ -0,0 +1,29 @@
+//declara las variables globales
+var formulario = document.getElementById('mod_equipo4');
+
+
+console.log('hola mundo4');
+
+formulario.addEventListener('submit', function (e)
+{
+    e.preventDefault();
+
+
+    var datos= new FormData(formulario);
+    //C:\version7\htdocs\Huitzila\Proyecto_Huitzila\controller\ejemplo\registro_ejemplo.php
+    fetch('../../controller/ejemplo/registro_equipo4.php', {
+        method: 'POST',
+        body: datos
+    })
+
+    .then(res => res.json())
+    .then(data => {
+        console.log(data);
+        if (data === 'exito') {
+            const form= document.getElementById('Equipo4');
+            form.reset();
+            alert("Registro exitoso");
+            
+        }
+    })
+})
