0596e1bb4e0d162d2046fad8f7aefdb82643b7b8
oscar23w <63673374+oscar23w@users.noreply.github.com>
2024-02-20 13:13:54 -0600

    Cambios para insertar los datos extraidos de la base de datos a los campos del formulario de modificar del equipo4

--- controller/ejemplo/js/Modificar2_Equipo4.js
+++ controller/ejemplo/js/Modificar2_Equipo4.js
@@ -1,14 +1,17 @@
 // Selecciona todos los enlaces de obtener información
 var linkObtenerInfo = document.querySelectorAll(".table_item__link.obtener-informacion");
 
-console.log(linkObtenerInfo);
-
 // Agrega el evento click a cada enlace de obtener información
 for (var i = 0; i < linkObtenerInfo.length; i++) {
     linkObtenerInfo[i].addEventListener('click', function(e) {
         e.preventDefault();
         var id = this.getAttribute('data-id');
         console.log(id);
+        console.log("!");
+       
+
+        // Guarda el ID en el localStorage
+        localStorage.setItem('id', id);
 
         // Realiza una solicitud fetch para enviar el ID a un script PHP
         fetch('../../controller/ejemplo/Modificar2_Equipo4.php?id=' + id, {
@@ -18,13 +21,19 @@ for (var i = 0; i < linkObtenerInfo.length; i++) {
             if (!response.ok) {
                 throw new Error('Network response was not ok');
             }
-            return response.json(); 
+            return response.json(); // Parsea la respuesta como JSON
         })
         .then(function(data) {
+            // Maneja la respuesta del PHP
             console.log(data);
+            // Convierte el objeto JSON en una cadena de texto y guárdalo en el localStorage
+            localStorage.setItem('data', JSON.stringify(data));
+            // Redirige a la página Mod_Equipo4.html
+            window.location.href = '../../view/ejemplo/Mod_Equipo4.html';
         })
         .catch(function(error) {
             console.error('There has been a problem with your fetch operation:', error);
         });
+        
     });
 }
