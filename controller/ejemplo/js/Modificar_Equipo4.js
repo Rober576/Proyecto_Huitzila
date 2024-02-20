5eabc92a2cb1b158273d2f43d12b0d444c625c36
KaronteElPro <alanaceistopherrr1@outlook.com>
2024-02-19 23:01:21 -0600

    mod

--- controller/ejemplo/js/Modificar_Equipo4.js
+++ controller/ejemplo/js/Modificar_Equipo4.js
@@ -1,13 +1,12 @@
 //declara las variables globales
-var formulario = document.getElementById('mod_equipo4');
+var formulario = document.getElementById('Equipo4');
 
 
-console.log('hola mundo4');
+
 
 formulario.addEventListener('submit', function (e)
 {
     e.preventDefault();
-
 
     var datos= new FormData(formulario);
     //C:\version7\htdocs\Huitzila\Proyecto_Huitzila\controller\ejemplo\registro_ejemplo.php
@@ -20,9 +19,9 @@ formulario.addEventListener('submit', function (e)
     .then(data => {
         console.log(data);
         if (data === 'exito') {
-            const form= document.getElementById('mod_equipo4');
+            const form= document.getElementById('Equipo4');
             form.reset();
-            alert("Registro exitoso");
+            
             
         }
     })
