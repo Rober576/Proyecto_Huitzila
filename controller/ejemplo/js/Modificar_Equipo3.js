07c36ff6ae5c62228af70a1d521912e60d0c6c71
Carlos Nolasco <cn8411@gmail.com>
2024-02-24 11:32:40 -0600

    correcion en el nombre del equipo 3(ya no permite numeros los borra automaticamente)

--- controller/ejemplo/js/Modificar_Equipo3.js
+++ controller/ejemplo/js/Modificar_Equipo3.js
@@ -186,7 +186,7 @@ mod_equipo3.campo4.addEventListener('keyup', (e) => {
     mod_equipo3.campo4.value = valorInput
 
     //elimina caracteres especiales
-    .replace(/[☺☻♥♦•○◙♂♀üâäàåçê♪ëèïîìÄÅÉæÆôöòûùÿÖÜ¢£¥₧ƒª`´·¨°º¿⌐¬½¼«»÷±~!¡#$%^&^*()+\=\[\]{};':"\\|,<>\/?]/g, '')
+    .replace(/[0-9☺☻♥♦•○◙♂♀üâäàåçê♪ëèïîìÄÅÉæÆôöòûùÿÖÜ¢£¥₧ƒª`´·¨°º¿⌐¬½¼«»÷±~!¡#$%^&^*()+\=\[\]{};':"\\|,<>\/?@]/g, '')
     
     
 
