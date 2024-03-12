e2199a1eb20ea4d7c72ac76807dda7a460ed9b4d
oscar23w <63673374+oscar23w@users.noreply.github.com>
2024-03-12 16:35:26 -0600

    Funciones para verificar que no exista un predio con el mismo código y si existe pedirle al usuario que cambie el código antes de registrar

--- controller/Agricola/Predios/js/Registrar_Predio.js
+++ controller/Agricola/Predios/js/Registrar_Predio.js
@@ -6,24 +6,37 @@ console.log('registro predio');
 formulario.addEventListener('submit', function (e)
 {
     e.preventDefault();
-
     var datos= new FormData(formulario);
-    fetch('../../../controller/Agricola/Predios/php/Registrar_Predio.php', {
+    
+    /* Se envian los datos de formulario para verificar que el código del predio no este siendo utilizado
+    en otro predio, en caso de que si mandar una alerta informando esto */
+    fetch('../../../controller/Agricola/Predios/php/Verificar_Codigo_Predio.php', {
         method: 'POST',
         body: datos
     })
-
     .then(res => res.json())
     .then(data => {
         console.log(data);
-        if (data === 'exito') {
-            const form= document.getElementById('advanced-form');
-            form.reset();
-            alert("Registro de predio exitoso");
-            
+        if (data === 'codigoUsado') {
+            alert("La clave del predio ya fue registrada anteriormente");     
         }
+        //si el codigo de predio no esta siendo utilizado se procede con el registro
         else{
-            alert("La clave de predio ya existe");
+            fetch('../../../controller/Agricola/Predios/php/Registrar_Predio.php', {
+                method: 'POST',
+                body: datos
+            })
+        
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                if (data === 'exito') {
+                    const form= document.getElementById('advanced-form');
+                    form.reset();
+                    alert("Registro de predio exitoso");
+                    
+                }
+            })
         }
     })
 })
