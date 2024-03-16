ad330621a505618bc840668ff6a5173a8aa15168
Alfonso758 <alfonsofrausto75@gmail.com>
2024-03-15 23:52:47 -0600

    editar usuario haciendo la busqueda de la clave

---
+++ controller/Usuarios/js/Buscar_Usuario.js
@@ -0,0 +1,23 @@
+var formulario = document.getElementById('clave');
+
+formulario.addEventListener('submit', function (e)
+{
+    e.preventDefault();
+
+    var datos= new FormData(formulario);
+    fetch('../../controller/Usuarios/Registro_Usuario.php', {
+        method: 'POST',
+        body: datos
+    })
+
+    .then(res => res.json())
+    .then(data => {
+        console.log(data);
+        if (data === 'exito') {
+            const form= document.getElementById('advanced-form');
+            form.reset();
+            alert("Registro exitoso");
+            
+        }
+    })
+})
