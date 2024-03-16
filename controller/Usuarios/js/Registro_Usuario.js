980e0390a5b69b9b35812f1826f03b26c7d30407
DiegoArte <94568408+DiegoArte@users.noreply.github.com>
2024-03-15 21:50:32 -0600

    Registro en proceso

--- controller/Usuarios/js/Registro_Usuario.js
+++ controller/Usuarios/js/Registro_Usuario.js
@@ -5,7 +5,6 @@ formulario.addEventListener('submit', function (e)
     e.preventDefault();
 
     var datos= new FormData(formulario);
-    //C:\version7\htdocs\Huitzila\Proyecto_Huitzila\controller\ejemplo\registro_ejemplo.php
     fetch('../../controller/Usuarios/Registro_Usuario.php', {
         method: 'POST',
         body: datos
