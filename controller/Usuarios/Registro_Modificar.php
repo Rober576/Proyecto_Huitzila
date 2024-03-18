e0d56b142e6758f97e05476038d55a97aa57f606
Alfonso758 <alfonsofrausto75@gmail.com>
2024-03-18 09:20:23 -0600

    Correción en modificar usuarios

---
+++ controller/Usuarios/Registro_Modificar.php
@@ -0,0 +1,15 @@
+<?php
+include_once("../../model/Usuarios/Registro_Modificar.php");
+// Configuración de la conexión a la base de datos (reemplaza estos valores con los tuyos)
+
+// Obtener el ID enviado desde JavaScript
+$id = $_GET['id'];
+
+$conn = new ObtenerCampos();
+$conn->instancias();
+$result = $conn->obtener($id);
+
+echo json_encode($result);
+
+// Cerrar conexión
+?>
