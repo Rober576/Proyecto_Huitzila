b9e255344c5ac573b9a7aac326b3957d7668a39c
LAMAGA2103 <magalycastillogomez259@gmail.com>
2024-03-20 11:43:08 -0600

    mostar datos que vienen en id en modificar

--- controller/Produccion/Get_Mezcal.php
+++ controller/Produccion/Get_Mezcal.php
@@ -8,10 +8,10 @@ $resultado = $objeto->buscar_datos($id);
 $lote = $resultado[0]['Lote'];
 $especie = $resultado[0]['NombrePlanta'];
 $tanque = $resultado[0]['Tanque'];
-
-$clase = $resultado[0]['IDClase'];
+$clase = $resultado[0]['Clase_Mezcal'];
+echo $clase;
 $edad = $resultado[0]['Edad'];
-$categoria = $resultado[0]['IDCategoria'];
+$categoria = $resultado[0]['Categoria'];
         
 ?>
 <script language="javascript">
@@ -19,10 +19,13 @@ $categoria = $resultado[0]['IDCategoria'];
         var selectEspecie = document.getElementById('especie');
         var especie = "<?php echo $especie ?>";
         var selectCategoria = document.getElementById('categoria');
-        var categoria = "<?php echo $categoria?>";
+       
+        var categoria = "<?php echo $categoria ?>"
+        console.log(categoria)
         var selectClases = document.getElementById('clase');
         var clase = "<?php echo $clase ?>";
 
+        
 
         fetch('Obtener_Categorias_Clase_Especie.php?tipo=especies')
             .then(response => response.json())
@@ -31,12 +34,17 @@ $categoria = $resultado[0]['IDCategoria'];
                     var option = document.createElement('option');
                     option.value = item;
                     option.textContent = item;
+                    console.log('Item:', item);
+                    console.log('especie:', especie);
+
 
                     if (selectEspecie) {
                         selectEspecie.appendChild(option);
+                        
                     }
                     if (item == especie) {
                         selectEspecie.value = especie;
+
                     }
                 });
             })
@@ -49,38 +57,42 @@ $categoria = $resultado[0]['IDCategoria'];
                     var option = document.createElement('option');
                     option.value = item;
                     option.textContent = item;
+                    console.log('Item:', item);
+                    console.log('Categoria:', categoria);
 
                     if (selectCategoria) {
                         selectCategoria.appendChild(option);
+                       
                     }
-                    if (item == categoria) {
+                    if (categoria === item) {
                         selectCategoria.value = categoria;
+                        console.log("entrocategoria");
                     }
                 });
             })
+
         fetch('Obtener_Categorias_Clase_Especie.php?tipo=clases')
-            .then(response => response.json())
-            .then(data => {
-                data.forEach(item => {
-                    var option = document.createElement('option');
-                    option.value = item;
-                    option.textContent = item;
+        .then(response => response.json())
+        .then(data => {
+            data.forEach(item => {
+                var option = document.createElement('option');
+                option.value = item;
+                option.textContent = item;
+                console.log('Item:', item);
+                console.log('Clase:', clase);
 
-                    if (selectClases) {
-                        selectClases.appendChild(option);
-                    }
-                    if (item == clase) {
-                        selectClases.value = clase;
-                    }
-                });
-            })
-            .catch(error => console.error('Error al obtener especies:', error));
-
+                if (selectClases) {
+                    selectClases.appendChild(option);
+                }
+                if (item == clase) {
+                    selectClases.value = clase;
+                }
+            });
+        })
         
 });
     document.getElementById("lote").value = "<?php echo $id ?>";
     document.getElementById("lote").readOnly = true;
-    
     document.getElementById("tanque").value = "<?php echo $tanque ?>";
     document.getElementById("edad").value = "<?php echo $edad ?>";
    
