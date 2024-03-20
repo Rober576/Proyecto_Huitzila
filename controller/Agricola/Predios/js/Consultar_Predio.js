845f7e49230b5c77810b45f6dde8ea5dbcd0ef6c
Ahira <tanibugarin@gmail.com>
2024-03-19 18:09:03 -0600

    modificacion en mostrar quitando el ajax

--- controller/Agricola/Predios/js/Consultar_Predio.js
+++ controller/Agricola/Predios/js/Consultar_Predio.js
@@ -1,28 +1,79 @@
+function buscar_datos(consulta) {
+    var url = '../../../controller/Agricola/Predios/php/Consultar_Predio.php';
 
-$(buscar_datos());
-function buscar_datos(consulta){
-    $.ajax({
-        url: '../../../controller/Agricola/Predios/php/Consultar_Predio.php',
-        type: 'POST',
-        dataType: 'html',
-        data: { consulta: consulta },
-    })
+    if (consulta !== "") {
+        url += '?consulta=' + consulta;
+    }
 
-        .done(function (respuesta)
-        {
-            $("#tabla").html(respuesta);
-        })
-        .fail(function ()
-        {
-            console.log("error");
-        })
+    // Crear una nueva instancia de XMLHttpRequest
+    var xhr = new XMLHttpRequest();
+    // Configurar la solicitud GET
+    xhr.open('GET', url, true);
+
+    // Definir la función de retorno de llamada (callback) para manejar la respuesta
+    xhr.onload = function() {
+        if (xhr.status === 200) {
+            document.getElementById("tabla").innerHTML = xhr.responseText;
+            EventoEditar();
+        } else {
+            console.error("Error al realizar la solicitud:", xhr.statusText);
+        }
+    };
+
+    xhr.onerror = function() {
+        console.error("Error al realizar la solicitud.");
+    };
+
+
+    xhr.send();
 }
 
-$(document).on('keyup', '#busqueda', function (){
-    var valorBusqueda = $(this).val();
-    if (valorBusqueda != "") {
-        buscar_datos(valorBusqueda);
-    } else {
-        buscar_datos();
+function EventoEditar() {
+    var botonesModificar = document.querySelectorAll(".boton-modificar");
+    // Agrega el evento click a cada botón de modificar
+    for (var j = 0; j < botonesModificar.length; j++) {
+        botonesModificar[j].addEventListener('click', function(e) {
+            // Redirige a una URL, por ejemplo:
+            window.location.href = "#";
+            
+            // Imprime la ID del botón en la consola
+            console.log("ID del botón:", e.target.id);
+
+            // Realiza el proceso que quieres que se haga
+            var id = e.target.id; // Obteniendo la ID del botón
+
+            localStorage.setItem('id', id);
+
+            fetch('../../../controller/Agricola/Predios/php/Modificar_Datos_Predio.php?id=' + id, {
+                method: 'GET',
+            })
+            .then(function(response) {
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                return response.json(); 
+            })
+            .then(function(data) {
+                // Convierte el objeto JSON en una cadena de texto y guárdalo en el localStorage
+                localStorage.setItem('data', JSON.stringify(data));
+                // Redirige a la página Mod_Equipo4.html
+                window.location.href = '../../../view/Agricola/Predios/Editar_Predios.html';
+            })
+            .catch(function(error) {
+                console.error('There has been a problem with your fetch operation:', error);
+            });
+        });
     }
-})
+}
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    buscar_datos("");
+});
+
+document.addEventListener('keyup', function(event) {
+    if (event.target.id === 'busqueda') {
+        var valorBusqueda = event.target.value.trim(); 
+        buscar_datos(valorBusqueda);
+    }
+});
