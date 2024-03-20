335ea0f8199b782f93f5871fd8bc24b0b9d4e5ac
Jorge Luis <luissanodvalrod727@gmail.com>
2024-03-20 00:08:44 -0600

    Ayudas en registrar insumos

--- view/insumos/js/Cancela_Reg_Insumos.js
+++ view/insumos/js/Cancela_Reg_Insumos.js
@@ -1,10 +1,19 @@
-// Agregar evento de click al botón Cancelar
 document.getElementById('Cancelar').addEventListener('click', function() {
-    // Obtener todos los campos del formulario
-    const campos = document.querySelectorAll('.formulario-datos input, .formulario-datos textarea');
+    // Mostrar un mensaje de confirmación
+    const confirmacion = confirm("¿Estás seguro de cancelar el registro? Los cambios no se guardarán.");
 
-    // Iterar sobre los campos y establecer su valor como cadena vacía
-    campos.forEach(function(campo) {
-        campo.value = "";
-    });
+    // Si el usuario confirma la cancelación
+    if (confirmacion) {
+        // Obtener todos los campos del formulario
+        const campos = document.querySelectorAll('.formulario-datos input, .formulario-datos textarea');
+        // Iterar sobre los campos y establecer su valor como cadena vacía
+        campos.forEach(function(campo) {
+            campo.value = "";
+        });
+        location.reload();
+    } else {
+        // Si el usuario cancela la acción, no hacer nada
+        return;
+    }
 });
+
