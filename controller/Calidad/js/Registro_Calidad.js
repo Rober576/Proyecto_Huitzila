9f8f7fb53aa02a0b5db87c00cb2a34e358d1d1fe
DiegoArte <94568408+DiegoArte@users.noreply.github.com>
2024-04-07 23:50:46 -0600

    Registro calidad en proceso

--- controller/Calidad/js/Registro_Calidad.js
+++ controller/Calidad/js/Registro_Calidad.js
@@ -3,20 +3,23 @@ var formulario = document.getElementById('advanced-form');
 formulario.addEventListener('submit', function (e)
 {
     e.preventDefault();
-
+    
     var datos= new FormData(formulario);
     fetch('../../controller/Calidad/Registro_Calidad.php', {
         method: 'POST',
         body: datos
     })
-
     .then(res => res.json())
     .then(data => {
+        console.log("Registro exitoso2");
         console.log(data);
-        if (data === 'exito') {
+        if (data.status === 'exito') {
             const form= document.getElementById('advanced-form');
             form.reset();
             alert("Registro exitoso");
         } 
     })
+    .catch(error => {
+        console.error('Error en la solicitud:', error);
+    });
 })
---
+++ view/Calidad/js/Validacion_Fisioquimico.js
@@ -0,0 +1,80 @@
+let bandera1=false;
+let bandera2=false;
+let bandera3=false;
+
+let botonRegistrar = document.getElementById("submitButton");
+botonRegistrar.addEventListener("click", (e) =>{
+    if(bandera1 == true && bandera2 == true && bandera3 == true){
+        console.log("Registro exitoso");
+    }
+
+    else{
+        alert("Datos incorrectos");
+        e.preventDefault();
+    }
+})
+
+const expresion = {
+    Azucares: /^\d{1,8}(\.\d{1,2})$/,
+    Madurez: /^\d{1,8}(\.\d{1,2})$/,
+    Materia: /^\d{1,8}(\.\d{1,2})$/,
+}
+
+var azucares = document.getElementById('azucares');
+var madurez = document.getElementById('madurez');
+var mat_prima = document.getElementById('mat_prima');
+azucares.addEventListener('keyup', (e) => {
+    let valorInput = e.target.value;
+    azucares.value = valorInput
+
+    .replace(/[^0-9.]/g, '')
+    .trim();
+
+
+    if (!expresion.Azucares.test(valorInput)) {
+        azucares.style.border = "5px solid red";
+        bandera1 = false;
+    }
+
+    else {
+        azucares.removeAttribute("style");
+        bandera1 = true;
+    }
+});
+
+madurez.addEventListener('keyup', (e) => {
+    let valorInput = e.target.value;
+
+    madurez.value = valorInput
+
+    .replace(/[^0-9.]/g, '')
+    .trim();
+
+
+    if (!expresion.Madurez.test(valorInput)) {
+        madurez.style.border = "5px solid red";
+        bandera2 = false;
+    } else {
+        madurez.removeAttribute("style");
+        bandera2 = true;
+    }
+});
+
+mat_prima.addEventListener('keyup', (e) => {
+    let valorInput = e.target.value;
+
+    mat_prima.value = valorInput
+
+    .replace(/[^0-9.]/g, '')
+    .trim();
+
+   
+
+    if (!expresion.Materia.test(valorInput)) {
+        mat_prima.style.border = "5px solid red";
+        bandera3 = false;
+    } else {
+        mat_prima.removeAttribute("style");
+        bandera3 = true;
+    }
+});
