72a7057a8750f0dc3f7883ce7badb59275f9d74c
Alfonso758 <alfonsofrausto75@gmail.com>
2024-04-09 19:55:00 -0600

    colores en mostrar fisioquimico

--- controller/Calidad/Mostrar_Fisicoquimico.php
+++ controller/Calidad/Mostrar_Fisicoquimico.php
@@ -12,6 +12,17 @@ if (isset($_POST['consulta'])) {
     //echo "No se envio";
 }
 
+echo"
+<table border='1'>
+<thead>
+    <tr>
+        <td class='fila-verde'>Cumple</td>
+        <td class='fila-rojo'>No cumple</td>
+        <td class='fila-naranja'>Pendiente</td>
+    </tr>
+</thead>
+</table> <br>";
+
 $salida = '';
 
 if ($resultado) {
@@ -34,19 +45,28 @@ if ($resultado) {
             <tbody>';
 
     foreach ($resultado as $fila) {
+        // Asignar una clase CSS condicionalmente según el valor de $fila['cumplimiento']
+        if ($fila['cumplimiento'] == 1) {
+            $clase_css = 'fila-verde';
+        } if ($fila['cumplimiento'] == 2) {
+            $clase_css = 'fila-rojo';
+        } if ($fila['cumplimiento'] == 0) {
+            $clase_css = 'fila-naranja';
+        }
+
         $id = $fila['NombreDocumento'];
         $salida .= '<tr>';
-        $salida .= '<td>' . $fila["Lote"] . '</td>';
-        $salida .= '<td>' . $fila["Alcohol"] . '</td>';
-        $salida .= '<td>' . $fila["ExtractoSeco"] . '</td>';
-        $salida .= '<td>' . $fila["Metanol"] . '</td>';
-        $salida .= '<td>' . $fila["AlcoholesSuperiores"] . '</td>';
-        $salida .= '<td>' . $fila["Aldehidos"] . '</td>';
-        $salida .= '<td>' . $fila["Furfural"] . '</td>';
-        $salida .= '<td>' . $fila["Plomo"] . '</td>';
-        $salida .= '<td>' . $fila["Cobre"] . '</td>';
+        $salida .= '<td class="' . $clase_css . '">' . $fila["Lote"] . '</td>';
+        $salida .= '<td class="' . $clase_css . '">' . $fila["Alcohol"] . '</td>';
+        $salida .= '<td class="' . $clase_css . '">' . $fila["ExtractoSeco"] . '</td>';
+        $salida .= '<td class="' . $clase_css . '">' . $fila["Metanol"] . '</td>';
+        $salida .= '<td class="' . $clase_css . '">' . $fila["AlcoholesSuperiores"] . '</td>';
+        $salida .= '<td class="' . $clase_css . '">' . $fila["Aldehidos"] . '</td>';
+        $salida .= '<td class="' . $clase_css . '">' . $fila["Furfural"] . '</td>';
+        $salida .= '<td class="' . $clase_css . '">' . $fila["Plomo"] . '</td>';
+        $salida .= '<td class="' . $clase_css . '">' . $fila["Cobre"] . '</td>';
 
-        $salida .= '<td>';
+        $salida .= '<td class="' . $clase_css . '">';
         $salida .= '<button type="submit" onclick="window.open(\'../../controller/Calidad/Abrir_Fisioquimico.php?id=' . $id . '\', \'_blank\')">Abrir</button>';
         $salida .= '</td>';
 
