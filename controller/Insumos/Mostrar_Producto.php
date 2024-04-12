1a9c2f49800fd7a23cfb2c3212d07ee959159bb5
Ana Isabel García Llamas <cuentaderespaldogit@gmail.com>
2024-04-12 09:06:27 -0600

    mostrar producto con búsqueda inteligente integrada

---
+++ controller/Insumos/Mostrar_Producto.php
@@ -0,0 +1,44 @@
+<?php
+include_once('../../model/Insumos/Mostrar_Producto.php');
+
+$salida = "";
+$base = new Mostrar;
+$base->instancias();
+$datos = [];
+
+// Verifica si se recibió un valor para la consulta
+if(isset($_POST['consulta'])) {
+    // Recibe el valor de la consulta
+    $consulta = $_POST['consulta'];
+
+    if($consulta == 'undefined'){
+        $resultados = $base->busqueda();
+
+    }
+
+    else{
+        $resultados = $base->busqueda($consulta);
+
+    }
+
+    $producto = ['id', 'des', 'max', 'min', 'cp', 'uc', 'can'];
+
+    for($i = 0; $i < count($resultados); $i++){
+        $producto[0] = $resultados[$i]["IDProducto"];
+        $producto[1] = $resultados[$i]["Descripcion"];
+        $producto[2] = $resultados[$i]["StockMinimo"];
+        $producto[3] = $resultados[$i]["StockMaximo"];
+        $producto[4] = $resultados[$i]["CostoProm"];
+        $producto[5] = $resultados[$i]["CostoUltimo"];
+        $producto[6] = $resultados[$i]["Cantidad"];
+        array_push($datos, $producto);
+    }
+
+    echo json_encode($datos);
+
+    
+} else {
+    // Si no se recibió ninguna consulta, puedes devolver un mensaje de error o manejar la situación según tus necesidades
+    echo json_encode("No se recibió ninguna consulta.");
+}
+?>
---
+++ view/insumos/js/Mostrar_Producto.js
@@ -0,0 +1,110 @@
+const cuerpo_tabla = document.getElementById("cuerpo");
+const contenedor_tabla = document.getElementById("tablaResultado")
+contenedor_tabla.style.display = 'none';
+
+function buscar_datos(consulta) {
+    // Construye los parámetros de la consulta
+    const params = new URLSearchParams();
+    params.append('consulta', consulta);
+
+    // Realiza la solicitud fetch
+    fetch('../../controller/Insumos/Mostrar_Producto.php', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        body: params
+    })
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        return response.text();
+    })
+    .then(respuesta => {
+        //document.getElementById('tablaResultado').innerHTML = respuesta;
+        console.log(respuesta)
+        mostrarDatos(JSON.parse(respuesta));
+    })
+    .catch(error => {
+        console.error('Hubo un problema con la solicitud:', error);
+    });
+}
+
+// Llama a buscar_datos() al cargar la página
+buscar_datos();
+
+// Escucha el evento keyup en el elemento con el ID 'busqueda'
+document.getElementById('buscar-txt').addEventListener('keyup', function() {
+    var valorBusqueda = this.value;
+    if (valorBusqueda != "") {
+        console.log(valorBusqueda)
+        buscar_datos(valorBusqueda);
+    } else {
+        buscar_datos();
+    }
+});
+
+function mostrarDatos(datos){
+    console.log(datos);
+    cuerpo_tabla.innerHTML = '';
+    
+
+    if(datos.length == 0){
+        tablaResultado.style.display = 'none';
+        mensaje.style.display = 'block';
+    }
+
+    else{
+        tablaResultado.style.display = 'block';
+        mensaje.style.display = 'none';
+
+        for(i = 0; i < datos.length; i++){
+            var row = document.createElement('tr');
+            var id_col = document.createElement('td');
+            var desc_col = document.createElement('td');
+            var cprom_col = document.createElement('td');
+            var ucost_col = document.createElement('td');
+            var cant_col = document.createElement('td');
+            var smax_col = document.createElement('td');
+            var smin_col = document.createElement('td');
+    
+            var acciones = document.createElement('td')
+            var link_eliminar = document.createElement('Button')
+            var link_editar = document.createElement('Button')
+    
+            console.log(datos[i]);
+            id_col.innerHTML = datos[i][0]
+            row.appendChild(id_col);
+            desc_col.innerHTML = datos[i][1];
+            row.appendChild(desc_col);
+            cprom_col.innerHTML = datos[i][2];
+            row.appendChild(cprom_col);
+            ucost_col.innerHTML = datos[i][3];
+            row.appendChild(ucost_col);
+            cant_col.innerHTML = datos[i][4];
+            row.appendChild(cant_col);
+            smax_col.innerHTML = datos[i][5];
+            row.appendChild(smax_col);
+            smin_col.innerHTML = datos[i][6];
+            row.appendChild(smin_col);
+            
+            link_eliminar.innerHTML = "Eliminar";
+            link_eliminar.href = "../../controller/Insumos/Eliminar_Producto.php?id="+datos[i][0];
+            acciones.appendChild(link_eliminar);
+    
+            link_editar.innerHTML = "Editar";
+            link_editar.href = "../../controller/Insumos/Modificar_Producto.php?id="+datos[i][0];
+            acciones.appendChild(link_editar);
+    
+            row.appendChild(acciones);
+            cuerpo_tabla.appendChild(row);
+            
+        }
+    }
+    
+
+    
+
+    
+}
