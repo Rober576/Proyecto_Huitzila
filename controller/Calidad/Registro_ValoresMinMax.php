bba7db4a6d10aefee64513dbdb59219636d137ad
Jaime88GC <jaimealejandrocasa83@gmail.com>
2024-04-13 18:43:09 -0600

    Avance del registro de valores minimos y maximos

---
+++ controller/Calidad/Registro_ValoresMinMax.php
@@ -0,0 +1,24 @@
+<?php
+include_once('../../model/Calidad/Registro_ValoresMinMax.php');
+$ID = $_POST["ID"];
+$ALCVolMin = $_POST["ALCVolMin"];
+$ALCVolMax = $_POST["ALCVolMax"];
+$ExtractoSecoMin = $_POST["ExtractoSecoMin"];
+$ExtractoSecoMax = $_POST["ExtractoSecoMax"];
+$MetanolMin = $_POST["MetanolMin"];
+$MetanolMax = $_POST["MetanolMax"];
+$AlcoholesSuperioresMin = $_POST["AlcoholesSuperioresMin"];
+$AlcoholesSuperioresMax = $_POST["AlcoholesSuperioresMax"];
+$AldehidosMin = $_POST["AldehidosMin"];
+$AldehidosMax = $_POST["AldehidosMax"];
+$FurfuralMin = $_POST["FurfuralMin"];
+$FurfuralMax = $_POST["FurfuralMax"];
+$PlomoMin = $_POST["PlomoMin"];
+$PlomoMax = $_POST["PlomoMax"];
+$ArsenicoMin = $_POST["ArsenicoMin"];
+$ArsenicoMax = $_POST["ArsenicoMax"];
+
+$calidad = new Registro();
+$calidad->conexion();
+$calidad->insertar($ID, $ALCVolMin, $ALCVolMax, $ExtractoSecoMin, $ExtractoSecoMax, $MetanolMin, $MetanolMax, $AlcoholesSuperioresMin, $AlcoholesSuperioresMax, $AldehidosMin, $AldehidosMax, $FurfuralMin, $FurfuralMax, $PlomoMin, $PlomoMax, $ArsenicoMin, $ArsenicoMax);
+?>
---
+++ model/Calidad/Registro_ValoresMinMax.php
@@ -0,0 +1,25 @@
+<?php
+    include('../../config/Crud_bd.php');
+
+    class Registro_Calidad{
+        private $base;
+    
+        function conexion(){
+            $this->base = new Crud_bd();
+            $this->base->conexion_bd();
+        }
+    
+        function insertar($ID, $ALCVolMin,$ALCVolMax, $ExtractoSecoMin, $ExtractoSecoMax, $MetanolMin, $MetanolMax, $AlcoholesSuperioresMin, $AlcoholesSuperioresMax , $FurfuralMin, $FurfuralMax, $PlomoMin, $PlomoMax, $ArsenicoMin, $ArsenicoMax){
+
+            $q1 = "INSERT INTO valoresminmax (ID, ALCVolMin, ALCVolMax, ExtractoSecoMin, ExtractoSecoMax, MetanolMin, MetanolMax) VALUES(:ID, :ALCVolMin, :ALCVolMax, :ExtractoSecoMin, :ExtractoSecoMax, :MetanolMin, :MetanolMax, ;AlcoholesSuperioresMin, :AlcoholesSuperioresMax, , :FurduralMin, :FurfuralMax,  :PlomoMin, :PlomoMax,:ArsenicoMin, :ArsenicoMax, )";
+            $a1= [":ID"=>$ID, ":ALCVolMin"=>$ALCVolMin, ":ALCVolMax"=>$ALCVolMax, ":ExtractoSecoMin"=>$ExtractoSecoMin, ":ExtractoSecoMax"=>$ExtractoSecoMax , ":MeatanolMin"=>$MetanolMin, ":MetanolMax"=>$MetanolMax, ":AlcoholesSuperioesMin"=>$AlcoholesSuperioresMin, ":AlcoholesSuperioresMax"=>$AlcoholesSuperioresMax, ":FurfuralMin"=>$FurfuralMin, ":FurfuralMax"=>$FurfuralMax, ":PlomoMin"=>$PlomoMin, ":PlomoMax"=>$PlomoMax, ":ArsenicoMin"=>$ArsenicoMin, ":ArsenicoMax"=>$ArsenicoMax];
+            $querry = $q1;
+            $parametros = $a1;           
+
+            $this->base->insertar_eliminar_actualizar($querry, $parametros);
+            $this->base->cerrar_conexion();
+            echo json_encode('exito');
+        }
+    
+        
+    }
