7ef132afea63b1aaee805dcdbfc35eccc15c5f2e
luisfernando27 <LZ20020098@zacatecassur.tecnm.mx>
2024-04-15 12:59:42 -0600

    Ayuda en calidad

--- controller/Calidad/Registro_Calidad.php
+++ controller/Calidad/Registro_Calidad.php
@@ -1,12 +1,18 @@
 <?php
 include_once('../../model/Calidad/Registro_Calidad.php');
-$Lote = 'ASS';
-$Azucares = 10.5;
-$Madurez = 10.5;
-$Materia = 10.5;
-
 $calidad = new Registro_Calidad();
-$calidad->conexion();
-$calidad->insertar($Lote, $Azucares, $Madurez, $Materia);
+$Lote = 'ASS';
+$Azucares = 10;
+$Madurez = 10;
+$Materia = 10;
+
+$u=$calidad->insertar($Lote, $Azucares, $Madurez, $Materia);
+    if($u==true){
+        echo json_encode('exito');
+        
+    }else{
+        echo json_encode('no exito');
+        
+    }
 
 ?>
--- model/Calidad/Registro_Calidad.php
+++ model/Calidad/Registro_Calidad.php
@@ -1,24 +1,17 @@
 <?php
 include('../../config/Crud_bd.php');
 
-class Registro_Calidad{
-    private $base;
-
-    function conexion(){
-        $this->base = new Crud_bd();
-        $this->base->conexion_bd();
-    }
+class Registro_Calidad extends Crud_bd{
 
     function insertar($Lote, $Azucares, $Madurez, $Materia){
-
+        $this->conexion_bd();
         $q1 = "INSERT INTO calidad (Lote, Azucares, Madurez, TamañoMateria) VALUES(:Lote, :Azucares, :Madurez, :TamañoMateria)";
         $a1= [":Lote"=>$Lote, ":Azucares"=>$Azucares, ":Madurez"=>$Madurez, ":TamañoMateria"=>$Materia];
-        $querry = $q1;
-        $parametros = $a1;           
-
-        $this->base->insertar_eliminar_actualizar($querry, $parametros);
+        $querry = [$q1];
+        $parametros = [$a1];
+        $ejecucion=$this->insertar_eliminar_actualizar($querry, $parametros);
+        return $ejecucion;
         $this->base->cerrar_conexion();
-        echo json_encode('exito');
     }
 
     
