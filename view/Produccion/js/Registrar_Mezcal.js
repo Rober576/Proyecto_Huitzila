fe658a7cdff43a9c0e711bf08d70563c2a8ad900
AramisHS <mikunaka123@gmail.com>
2024-04-16 12:13:52 -0600

    cambia el label dependiendo de la combo box

--- view/Produccion/js/Registrar_Mezcal.js
+++ view/Produccion/js/Registrar_Mezcal.js
@@ -78,6 +78,48 @@ document.addEventListener('DOMContentLoaded', function() {
     .catch(error => console.error('Error al obtener especies:', error));
 });
 
+document.addEventListener('DOMContentLoaded', function() {
+    var selectClase = document.getElementById('clase');
+    var inputEdadLabel = document.querySelector('label[for="edad"]');
+    var inputEdad = document.getElementById('edad'); // Corregido para seleccionar por ID
+    var asterisco = document.querySelector('.campo-obligatorio');
+
+    // Se establecen los estilos iniciales del campo de entrada de la edad
+    inputEdad.disabled = true;
+    inputEdad.style.backgroundColor = 'lightgrey';
+
+    selectClase.addEventListener('change', function() {
+        var selectedOption = selectClase.value;
+        
+        // Si se selecciona "Blanco", se bloquea el campo de entrada de la edad y se borra su valor
+        if (selectedOption === "Blanco") {
+            inputEdadLabel.textContent = "Edad"; // Restaurar texto original del label
+            inputEdadLabel.classList.add('campo-bloqueado'); // Añadir clase para cambiar el estilo del label
+            asterisco.classList.remove('campo-obligatorio'); // Remover clase para quitar el asterisco rojo
+            inputEdadLabel.appendChild(asterisco); // Añadir asterisco al label
+            // Establecer el campo de entrada de la edad como deshabilitado y con un color de fondo gris claro
+            inputEdad.disabled = true;
+            inputEdad.style.backgroundColor = 'lightgrey';
+        } else {
+            // Cambiar el texto del label según la clase seleccionada
+            if (selectedOption === "Reposado") {
+                inputEdadLabel.textContent = "Meses";
+            } else if (selectedOption === "Añejo") {
+                inputEdadLabel.textContent = "Años";
+            }
+            asterisco.classList.add('campo-obligatorio'); // Añadir clase para el asterisco rojo
+            inputEdadLabel.appendChild(asterisco); // Añadir asterisco al label
+            inputEdadLabel.classList.remove('campo-bloqueado'); // Remover clase cuando el campo no está bloqueado
+            // Establecer el campo de entrada de la edad como habilitado y con un color de fondo blanco
+            inputEdad.disabled = false;
+            inputEdad.style.backgroundColor = 'white';
+        }
+    });
+});
+
+
+
+
 
 //responde cuando hay un click en el boton cancelar
 formulario.cancelar.addEventListener('click', function (e){
@@ -91,3 +133,4 @@ formulario.cancelar.addEventListener('click', function (e){
 
     
 })
+
