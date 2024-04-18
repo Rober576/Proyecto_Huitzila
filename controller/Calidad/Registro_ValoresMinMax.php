b8761872f7cd5a237d5574c9e13e28e91a0058db
DiegoArte <94568408+DiegoArte@users.noreply.github.com>
2024-04-18 11:22:24 -0600

    valores min max completado

--- controller/Calidad/Registro_ValoresMinMax.php
+++ controller/Calidad/Registro_ValoresMinMax.php
@@ -1,24 +1,23 @@
 <?php
 include_once('../../model/Calidad/Registro_ValoresMinMax.php');
-$ID = $_POST["ID"];
-$ALCVolMin = $_POST["ALCVolMin"];
-$ALCVolMax = $_POST["ALCVolMax"];
-$ExtractoSecoMin = $_POST["ExtractoSecoMin"];
-$ExtractoSecoMax = $_POST["ExtractoSecoMax"];
-$MetanolMin = $_POST["MetanolMin"];
-$MetanolMax = $_POST["MetanolMax"];
-$AlcoholesSuperioresMin = $_POST["AlcoholesSuperioresMin"];
-$AlcoholesSuperioresMax = $_POST["AlcoholesSuperioresMax"];
-$AldehidosMin = $_POST["AldehidosMin"];
-$AldehidosMax = $_POST["AldehidosMax"];
-$FurfuralMin = $_POST["FurfuralMin"];
-$FurfuralMax = $_POST["FurfuralMax"];
-$PlomoMin = $_POST["PlomoMin"];
-$PlomoMax = $_POST["PlomoMax"];
-$ArsenicoMin = $_POST["ArsenicoMin"];
-$ArsenicoMax = $_POST["ArsenicoMax"];
+$ALCVolMin = floatval($_POST["min_alcohol"]);
+$ALCVolMax = floatval($_POST["max_alcohol"]);
+$ExtractoSecoMin = floatval($_POST["min_ext_seco"]);
+$ExtractoSecoMax = floatval($_POST["max_ext_seco"]);
+$MetanolMin = floatval($_POST["min_metanol"]);
+$MetanolMax = floatval($_POST["max_metanol"]);
+$AlcoholesSuperioresMin = floatval($_POST["min_alc_sup"]);
+$AlcoholesSuperioresMax = floatval($_POST["max_alc_sup"]);
+$AldehidosMin = floatval($_POST["min_aldehidos"]);
+$AldehidosMax = floatval($_POST["max_aldehidos"]);
+$FurfuralMin = floatval($_POST["min_furfural"]);
+$FurfuralMax = floatval($_POST["max_furfural"]);
+$PlomoMin = floatval($_POST["min_plomo"]);
+$PlomoMax = floatval($_POST["max_plomo"]);
+$ArsenicoMin = floatval($_POST["min_arsenico"]);
+$ArsenicoMax = floatval($_POST["max_arsenico"]);
 
 $calidad = new Registro_MinMax();
 $calidad->conexion();
-$calidad->insertar($ID, $ALCVolMin, $ALCVolMax, $ExtractoSecoMin, $ExtractoSecoMax, $MetanolMin, $MetanolMax, $AlcoholesSuperioresMin, $AlcoholesSuperioresMax, $AldehidosMin, $AldehidosMax, $FurfuralMin, $FurfuralMax, $PlomoMin, $PlomoMax, $ArsenicoMin, $ArsenicoMax);
+$calidad->insertar($ALCVolMin, $ALCVolMax, $ExtractoSecoMin, $ExtractoSecoMax, $MetanolMin, $MetanolMax, $AlcoholesSuperioresMin, $AlcoholesSuperioresMax, $AldehidosMin, $AldehidosMax, $FurfuralMin, $FurfuralMax, $PlomoMin, $PlomoMax, $ArsenicoMin, $ArsenicoMax);
 ?>
