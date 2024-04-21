71b4197d3bd2a0d3e977e310c2c2bab27433926b
Retzat <ret.hatsu@gmail.com>
2024-04-21 15:35:20 -0600

    Archivos de sesiones por modulo

---
+++ view/insumos/js/Verificar_Permiso_Envasado.js
@@ -0,0 +1,24 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+    event.preventDefault();
+    console.log("Verificar_Permiso_Calidad.js");
+    document.body.style.display = "none";
+    let formulario_data = new FormData();
+    formulario_data.append("acceso_a", "3");
+    fetch("../../controller/login/Verificar_Sesion.php",
+        {
+            method: "POST",
+            body: formulario_data
+        })
+        .then(response => response.json())
+        .then(respuesta => {
+        
+            if (respuesta[0] == 0) {
+                window.location.href = '../../controller/login/Logout.php';
+            }else{
+                document.body.style.display = "block";
+            }
+
+        }).catch((error) => {
+            console.error('Error:', error);
+        });
+});
