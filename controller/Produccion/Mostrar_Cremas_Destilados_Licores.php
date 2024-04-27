6656b274f38abccc245455e84fd805a7f28db627
Chinonomon <chinonomon>
2024-04-27 16:27:10 -0600

    .

--- controller/Produccion/Mostrar_Cremas_Destilados_Licores.php
+++ controller/Produccion/Mostrar_Cremas_Destilados_Licores.php
@@ -46,7 +46,7 @@ if ($resultado) {
         $salida .= '<td>';
         $salida .= '<button  href="#"  class="boton-eliminar" type="submit" data-id="' . $Lote . '">Eliminar</button>';
         $salida .= ' ';
-        $salida .= '<button  onclick="window.location.href=\'../../controller/Produccion/Get_Mezcal.php?id='.$Lote.'\'"  class="boton-modificar" type="submit" data-id="' . $Lote . '">Modificar</button>';
+        $salida .= '<button  onclick="window.location.href="  class="boton-modificar" type="submit" data-id="' . $Lote . '">Modificar</button>';
         $salida .= '</td>';
         $salida .= '</tr>';
     }
