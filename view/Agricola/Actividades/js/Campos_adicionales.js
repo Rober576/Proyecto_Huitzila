32111fdd257b579d1fdaa0bfd966b10291bafe36
Barbarus1300 <fernanglezmaga@gmail.com>
2024-04-30 10:59:57 -0600

    cambio de nombre en un archivo, adicion de campos adicionales en el formulario de registro de actividades

--- view/Agricola/Actividades/js/Campos_adicionales.js
+++ view/Agricola/Actividades/js/Campos_adicionales.js
@@ -31,11 +31,6 @@ document.addEventListener('DOMContentLoaded', function() {
                     }    
                 }
             }
-            
-
-
-
-
 
         }else {
             // Si se selecciona 'No', mostrar solo la tercera sección y ocultar la segunda
@@ -57,3 +52,32 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 });
 
+document.addEventListener("DOMContentLoaded", function() {
+    var actividadSele = document.getElementById("actividadSele");
+    var campoTexto1 = document.getElementById("campo1");
+    var campoTexto2 = document.getElementById("campo2");
+
+    // Función para manejar la visibilidad de los campos adicionales
+    function toggleCamposAdicionales() {
+        // Verificar la opción seleccionada
+        if (actividadSele.value === "Control de plagas: Químico") {
+            // Mostrar ambos campos si se selecciona "Control de plagas: Químico"
+            campoTexto1.style.display = "block";
+            campoTexto2.style.display = "block";
+        } else if (actividadSele.value === "Control de plagas: Biológico") {
+            // Mostrar solo el primer campo si se selecciona "Control de plagas: Biológico"
+            campoTexto1.style.display = "block";
+            campoTexto2.style.display = "none";
+        } else {
+            // Ocultar ambos campos si no se selecciona ninguna opción específica
+            campoTexto1.style.display = "none";
+            campoTexto2.style.display = "none";
+        }
+    }
+
+    // Llamar a la función para manejar la visibilidad de los campos adicionales cuando se carga la página
+    toggleCamposAdicionales();
+
+    // Agregar evento de cambio al select
+    actividadSele.addEventListener("change", toggleCamposAdicionales);
+});
