eb6b979e299956fd03e2614529e572ef7f3f2e1b
DiegoArte <94568408+DiegoArte@users.noreply.github.com>
2024-05-05 11:18:58 -0600

    Correccion de codigo

--- controller/Recuperar_Contraseña/js/Codigo_Recupera_Contra.js
+++ controller/Recuperar_Contraseña/js/Codigo_Recupera_Contra.js
@@ -1,3 +1,18 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var correo = sessionStorage.getItem('correo');
+    var codigo = sessionStorage.getItem('codigo');
+
+    if (correo && codigo) {
+        document.getElementById('correo').value = correo;
+        document.getElementById('codigo2').value = codigo;
+    } else {
+        console.log('No se proporcionaron el correo electrónico y/o el código de recuperación.');
+    }
+
+    sessionStorage.removeItem('correo');
+    sessionStorage.removeItem('codigo');
+});
+
 var formulario = document.getElementById('formulario');
 
 formulario.addEventListener('submit', function (e)
