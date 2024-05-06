5dec6bcf946227abb6459137164dc4abd10077fe
Alfonso758 <alfonsofrausto75@gmail.com>
2024-05-06 14:44:17 -0600

    cambie el correo para mandar los codigos de verificación

--- controller/Recuperar_Contraseña/Recuperar_Contra_1.php
+++ controller/Recuperar_Contraseña/Recuperar_Contra_1.php
@@ -24,8 +24,8 @@ try {
     $mail->isSMTP();                                            //Send using SMTP
     $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-    $mail->Username   = 'frausto754537@gmail.com';                     //SMTP username
-    $mail->Password   = 'sqwp ftvv xzea npjc';                               //SMTP password
+    $mail->Username   = 'huitzila172@gmail.com';                     //SMTP username
+    $mail->Password   = 'lsru jtkv zxic rnqt';                               //SMTP password
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
     $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
