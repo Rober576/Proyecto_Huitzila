293dd17021701edde1626b00dfca58690f005bb0
LAMAGA2103 <magalycastillogomez259@gmail.com>
2024-05-07 11:09:06 -0600

    modificacion en movimientos mezcal

--- controller/Produccion/Modificar_Movimientos.php
+++ controller/Produccion/Modificar_Movimientos.php
@@ -9,22 +9,19 @@ $procedencia = $_POST["procedencia"];
 $movimiento = $_POST["mov"];
 $volumen = $_POST["volumen"];
 $volumen2 = $_POST["vol_agua"];
-
 $concentracion = $_POST["alc_vol"];
 $volumen3=$_POST["alc_vol55"];
 $alc_vol_merma = $_POST['alc_vol_merma'];
 $volumen_merma = $_POST['volumen_merma'];
-
-// Verificar si alc_vol55 y agua están definidos y no son nulos, y asignarles 0 si son nulos
 $volumen3 = isset($_POST['alc_vol55']) ? $_POST['alc_vol55'] : 0;
 $volumen2 = isset($_POST['vol_agua']) ? $_POST['vol_agua'] : 0;
 $alc_vol_merma = isset($_POST['alc_vol_merma']) ? $_POST['alc_vol_merma'] : 0;
 $volumen_merma = isset($_POST['volumen_merma']) ? $_POST['volumen_merma'] : 0;
-// Realiza la actualización con el ID de movimiento obtenido
 $u = $objeto->actualizar($lote2, $numero,$fecha, $tipo, $procedencia, $movimiento, $volumen, $volumen2, $concentracion,$volumen3,$alc_vol_merma,$volumen_merma);
 
 if ($u == true) {
     echo json_encode('exito');
+
 } else {
     echo json_encode('Error al actualizar el movimiento.');
 }
