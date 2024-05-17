8a4b4b037cb3a40d8275acf5208526cac453ce0a
LAMAGA2103 <magalycastillogomez259@gmail.com>
2024-05-17 08:03:40 -0600

    creacion de archivos para modificar movimientos en produccion

---
+++ controller/Produccion/Modificar_Movimientos_Cremas_Destilados_Licores.php
@@ -0,0 +1,28 @@
+<?php
+require_once('../../model/Produccion/Mod_Movimientos_Mezcal.php');
+$objeto = new modificarMez();
+$lote2 = $_POST["lote"];
+$numero =$_POST["numero"];
+$fecha = $_POST["fecha"];
+$tipo = $_POST["tipo"];
+$procedencia = $_POST["procedencia"];
+$movimiento = $_POST["mov"];
+$volumen = $_POST["volumen"];
+$volumen2 = $_POST["vol_agua"];
+$concentracion = $_POST["alc_vol"];
+$volumen3=$_POST["alc_vol55"];
+$alc_vol_merma = $_POST['alc_vol_merma'];
+$volumen_merma = $_POST['volumen_merma'];
+$volumen3 = isset($_POST['alc_vol55']) ? $_POST['alc_vol55'] : 0;
+$volumen2 = isset($_POST['vol_agua']) ? $_POST['vol_agua'] : 0;
+$alc_vol_merma = isset($_POST['alc_vol_merma']) ? $_POST['alc_vol_merma'] : 0;
+$volumen_merma = isset($_POST['volumen_merma']) ? $_POST['volumen_merma'] : 0;
+$u = $objeto->actualizar($lote2, $numero,$fecha, $tipo, $procedencia, $movimiento, $volumen, $volumen2, $concentracion,$volumen3,$alc_vol_merma,$volumen_merma,$numeroMovimiento);
+
+if ($u == true) {
+    echo json_encode('exito');
+
+} else {
+    echo json_encode('Error al actualizar el movimiento.');
+}
+?>
---
+++ controller/Produccion/js/Modificar_Movimientos_Cremas_Destilados_Licores.js
@@ -0,0 +1,68 @@
+var formulario = document.getElementById('form_ingreso_agave');
+var respuesta = document.getElementById('respuesta');
+console.log("entro a modificar_movimientos")
+// Responde cuando hay un click en el boton "Guardar"
+document.getElementById('submitButton').addEventListener('click', function (e) {
+    e.preventDefault(); // Prevenir el envío del formulario por defecto
+    console.log("Mensaje de prueba en la consola");
+    var datos = new FormData(formulario);
+    console.log("Antes de fetch"); // Agregado para depuración
+    
+    
+    var movimiento = obtenerParametroURL('NumeroMovimiento');
+    console.log(movimiento);
+
+    if (movimiento !== null && movimiento !== ""){
+        datos.append('numero', movimiento);
+    }
+
+
+    fetch('../../controller/Produccion/Modificar_Movimientos_Cremas_Destilados_Licores.php',
+    {
+        method: 'POST',
+        body: datos
+    })
+    .then(res => res.text()) // Cambiado a text() para manejar cualquier tipo de respuesta
+    .then(data => {
+        console.log("Después de fetch"); // Agregado para depuración
+        console.log("Mensaje de prueba",data); // Agregado para depuración
+        if (data.trim() === "id") {
+            alert("Hubo un error");
+        } else if(data.trim) {
+            alert("Actualización exitosa");
+            var parametrosURL = new URLSearchParams(window.location.search);
+            var lotE = parametrosURL.get('Lote');
+            console.log('lote',lotE)
+            // Redirigir a movimiento_espeficico.html con el parámetro 'lote'
+            //location.href = "../../view/Produccion/Movimiento_Especifico_Mezcal.html?Lote=" + lotE;
+          
+            }
+    })
+    .catch(error => {
+        console.error('Error en la solicitud fetch:', error); // Agregado para manejar errores de fetch
+        alert("Hubo un error en la solicitud"); // Muestra un mensaje de error genérico
+    });
+});
+
+// Responde cuando hay un click en el boton "Cancelar"
+formulario.cancelar.addEventListener('click', function (e){
+    e.preventDefault();
+    let urlAct = window.location+''
+
+    var resp = confirm("Los cambios realizados no se guardarán, ¿desea continuar?");
+    if(resp ==  true){
+        var parametrosURL = new URLSearchParams(window.location.search);
+        var lotE = parametrosURL.get('Lote');
+        console.log('lote',lotE)
+        // Redirigir a movimiento_espeficico.html con el parámetro 'lote'
+        //location.href = "../../view/Produccion/Movimiento_Especifico_Mezcal.html?Lote=" + lotE;
+     }
+
+    
+});
+
+
+function obtenerParametroURL(parametro) {
+    var parametrosURL = new URLSearchParams(window.location.search);
+    return parametrosURL.get(parametro);
+  }
