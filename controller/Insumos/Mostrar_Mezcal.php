b23adc5fead28f9374e8acdc1b8d46c0d657f7d9
LAMAGA2103 <magalycastillogomez259@gmail.com>
2024-05-19 16:38:36 -0600

    mostrar tabla en insumos que tenga analisisfisioquimico dentro de consultar mezcal en insumos

---
+++ controller/Insumos/Mostrar_Mezcal.php
@@ -0,0 +1,50 @@
+<?php
+include_once('../../model/Insumos/Mostrar_MezcalPM.php');
+
+
+$base = new MostrarMez();
+$base->instancias();
+if (isset($_POST['consulta'])) {
+    $busqueda = $_POST['consulta'];
+    $resultado = $base->buscador($busqueda);
+} else {
+    $resultado = $base->getInfo();
+}
+
+$salida = '';
+
+if ($resultado) {
+    $salida .= '
+        <table>
+            <thead>
+                <tr class="titulo_tabla">
+                    <th>Lote</th>
+                    <th>Especie</th>
+                    <th>Edad</th>
+                    <th>Categoría</th>
+                    <th>Volumen</th>
+                    <th>% Concentracion de alc.</th>
+                </tr>
+            </thead>
+
+        </table>';
+
+    foreach ($resultado as $fila) {
+    
+        $salida .= '<tr>';
+        $Lote= $fila['Lote'];
+        $salida .= '<td>' . $fila["Lote"] . '</td>';
+        $salida .= '<td>' .$fila["NombrePlanta"] . '</td>';
+        $salida .= '<td>' . ($fila["Edad"] == -1 ? '-' : $fila["Edad"]) . '</td>';
+        $salida .= '<td>' . $fila["Categoria"] . '</td>';
+        $salida .= '<td>' . $fila["Volumen"] . '</td>';
+        $salida .= '<td>' . $fila["PorcentajeAlcohol"] . '</td>';
+     
+        $salida .= '</tr>';
+    }
+    $salida .= '</tbody></table>';
+} else {
+    $salida .= 'No se encontraron resultados';
+}
+ echo $salida;
+?>
---
+++ controller/Insumos/js/Mostrar_MezcalP.js
@@ -0,0 +1,36 @@
+function buscar_datos(consulta) {
+    var url = '../../controller/Insumos/Mostrar_Mezcal.php';
+    console.log("entra al js");
+    if (consulta !== "") {
+        url += '?consulta=' + consulta;
+    }
+
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', url, true);
+
+    xhr.onload = function() {
+        if (xhr.status === 200) {
+            // Insertar los datos dentro del <tbody> del <table>
+            document.querySelector("#tablaResultado tbody").innerHTML = xhr.responseText;
+        } else {
+            console.error("Error al realizar la solicitud:", xhr.statusText);
+        }
+    };
+
+    xhr.onerror = function() {
+        console.error("Error al realizar la solicitud.");
+    };
+
+    xhr.send();
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    buscar_datos("");
+});
+
+document.addEventListener('keyup', function(event) {
+    if (event.target.id === 'busqueda') {
+        var valorBusqueda = event.target.value.trim(); 
+        buscar_datos(valorBusqueda);
+    }
+});
