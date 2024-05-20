78c84b85b85ff495b97680e138ede0bbe602c52b
KaronteElPro <alanaceistopherrr1@outlook.com>
2024-05-20 09:01:05 -0600

    intento de solucionar error en llenado actividades p1

--- controller/Agricola/Actividades/js/Consulta_Tipos_Actividades.js
+++ controller/Agricola/Actividades/js/Consulta_Tipos_Actividades.js
@@ -1,6 +1,12 @@
-
+// Define una función para inicializar las opciones en los selects
+function inicializarSelects() {
     var predioSelect = document.getElementById('predioSele');
-    fetch('../../../controller/Agricola/Actividades/php/Consultar_Tipos_Actividades.php?tipo=predios')
+    var actividadSele = document.getElementById('actividadSele');
+    var selePlaga = document.getElementById('selePlaga');
+    var seleHerbicida = document.getElementById('seleHerbicida');
+
+    // Fetch predios
+    let fetchPredios = fetch('../../../controller/Agricola/Actividades/php/Consultar_Tipos_Actividades.php?tipo=predios')
         .then(response => response.json())  
         .then(data => {
             data.forEach(item => {
@@ -14,9 +20,8 @@
         })
         .catch(error => console.error('Error al obtener predios:', error));
 
-    var actividadSele = document.getElementById('actividadSele');
-
-    fetch('../../../controller/Agricola/Actividades/php/Consultar_Tipos_Actividades.php?tipo=actividades')
+    // Fetch actividades
+    let fetchActividades = fetch('../../../controller/Agricola/Actividades/php/Consultar_Tipos_Actividades.php?tipo=actividades')
         .then(response => response.json())  
         .then(data => {
             data.forEach(item => {
@@ -28,58 +33,49 @@
                 }
             });
         })
-
         .catch(error => console.error('Error al obtener actividades:', error));
 
+    // Fetch plagas
+    let fetchPlagas = fetch('../../../controller/Agricola/Actividades/php/Consultar_Tipos_Actividades.php?tipo=plagas')
+        .then(response => response.json())  
+        .then(data => {
+            data.forEach(item => {
+                var option = document.createElement('option');
+                option.value = item;
+                option.textContent = item;
+                if (selePlaga) {
+                    selePlaga.appendChild(option);
+                }
+            });
 
+            var optionAgregar = document.createElement('option');
+            optionAgregar.value = 'agregar';
+            optionAgregar.textContent = 'Agregar nueva plaga';
+            selePlaga.appendChild(optionAgregar);
+        })
+        .catch(error => console.error('Error al obtener plagas:', error));
 
-    var selePlaga = document.getElementById('selePlaga');
+    // Fetch herbicidas
+    let fetchHerbicidas = fetch('../../../controller/Agricola/Actividades/php/Consultar_Tipos_Actividades.php?tipo=herbicidas')
+        .then(response => response.json())  
+        .then(data => {
+            data.forEach(item => {
+                var option = document.createElement('option');
+                option.value = item;
+                option.textContent = item;
+                if (seleHerbicida) {
+                    seleHerbicida.appendChild(option);
+                }
+            });
 
-    fetch('../../../controller/Agricola/Actividades/php/Consultar_Tipos_Actividades.php?tipo=plagas')
-    .then(response => response.json())  
-    .then(data => {
-        data.forEach(item => {
-            var option = document.createElement('option');
-            option.value = item;
-            option.textContent = item;
-            if (selePlaga) {
-                selePlaga.appendChild(option);
-            }
-        });
+            var optionAgregar = document.createElement('option');
+            optionAgregar.value = 'agregar';
+            optionAgregar.textContent = 'Agregar nuevo herbicida';
+            seleHerbicida.appendChild(optionAgregar);
+        })
+        .catch(error => console.error('Error al obtener herbicidas:', error));
 
-        var optionAgregar = document.createElement('option');
-        optionAgregar.value = 'agregar';
-        optionAgregar.textContent = 'Agregar nueva plaga';
-        selePlaga.appendChild(optionAgregar);
-    })
-
-    .catch(error => console.error('Error al obtener plagas:', error));
-
-
-    var seleHerbicida = document.getElementById('seleHerbicida');
-
-    fetch('../../../controller/Agricola/Actividades/php/Consultar_Tipos_Actividades.php?tipo=herbicidas')
-    .then(response => response.json())  
-    .then(data => {
-        data.forEach(item => {
-            var option = document.createElement('option');
-            option.value = item;
-            option.textContent = item;
-            if (seleHerbicida) {
-                seleHerbicida.appendChild(option);
-            }
-        });
-
-        var optionAgregar = document.createElement('option');
-        optionAgregar.value = 'agregar';
-        optionAgregar.textContent = 'Agregar nuevo herbicida';
-        seleHerbicida.appendChild(optionAgregar);
-    })
-
-    .catch(error => console.error('Error al obtener herbicidas:', error));
-
-
-
-
-
-    
+    // Esperar a que todas las peticiones fetch se completen
+    return Promise.all([fetchPredios, fetchActividades, fetchPlagas, fetchHerbicidas]);
+}
+inicializarSelects()
