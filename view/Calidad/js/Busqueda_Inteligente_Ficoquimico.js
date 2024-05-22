40dd966e3a03e8871fa29458240fec9dc21b52d6
Jaime88GC <jaimealejandrocasa83@gmail.com>
2024-05-21 20:05:52 -0600

    Cambios en barras de busqueda

--- view/Calidad/js/Busqueda_Inteligente_Ficoquimico.js
+++ view/Calidad/js/Busqueda_Inteligente_Ficoquimico.js
@@ -1,9 +1,22 @@
 $(document).ready(function () {
     $('#buscar-txt').on('keyup', function () {
         var value = $(this).val().toLowerCase();
-        $('#tablaResultado table tbody tr').filter(function () {
-            var textToSearch = $(this).children('td').slice(0, -2).text().toLowerCase();
+        var rows = $('#tablaResultado table tbody tr').filter(function () {
+            var textToSearch = $(this).children('td:first').text().toLowerCase();
             $(this).toggle(textToSearch.indexOf(value) > -1);
+            return $(this).is(':visible');
         });
+
+        if (rows.length === 0 && value !== "") {
+            $('#tablaResultado').hide(); // Ocultar la tabla solo si se ha ingresado texto de búsqueda
+            $('#no-results').show(); // Mostrar mensaje de no resultados
+        } else {
+            $('#tablaResultado').show(); // Mostrar la tabla si hay resultados
+            $('#no-results').hide(); // Ocultar mensaje de no resultados
+        }
     });
+
+    // Ocultar mensaje de no resultados y mostrar la tabla al cargar la página
+    $('#no-results').hide();
+    $('#tablaResultado').show();
 });
