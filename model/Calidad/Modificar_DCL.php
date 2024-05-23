bef599065ba183cbe6b245b74f7469eda672179f
DiegoArte <94568408+DiegoArte@users.noreply.github.com>
2024-05-22 22:28:19 -0600

    Editar DCL

--- model/Calidad/Modificar_DCL.php
+++ model/Calidad/Modificar_DCL.php
@@ -1,7 +1,7 @@
 <?php
 include('../../config/Crud_bd.php');
 
-class Registro_cuotas
+class Registro_DCL
 {
     private $base;
 
@@ -11,55 +11,39 @@ class Registro_cuotas
         $this->base->conexion_bd();
     }
 
-    function insertar($Clave, $Lote, $Alcohol, $Extracto, $Metanol, $Superiores, $Aldehidos, $Furfural, $Plomo, $Cobre, $archivo)
-    {
-       
-        $query = "SELECT * FROM valoresminmax";
-        $resultados = $this->base->mostrar($query);
-        $resultados=$resultados[0];
+    function insertar($Clave, $Lote, $Alcohol, $Metanol, $Superiores) {
+    $query = "SELECT * FROM valoresminmaxdcl";
+    $resultados = $this->base->mostrar($query);
+    $resultados = $resultados[0];
 
-        $cumplimiento=1;
-        if($Alcohol<$resultados['ALCVolMin'] || $Alcohol>$resultados['ALCVolMax']) {
-            $cumplimiento=2;
-        }
-        if($Extracto<$resultados['ExtractoSecoMin'] || $Extracto>$resultados['ExtractoSecoMax']) {
-            $cumplimiento=2;
-        }
-        if($Metanol<$resultados['MetanolMin'] || $Metanol>$resultados['MetanolMax']) {
-            $cumplimiento=2;
-        }
-        if($Superiores<$resultados['AlcoholesSuperioresMin'] || $Superiores>$resultados['AlcoholesSuperioresMax']) {
-            $cumplimiento=2;
-        }
-        if($Aldehidos<$resultados['AldehidosMin'] || $Aldehidos>$resultados['AldehidosMax']) {
-            $cumplimiento=2;
-        }
-        if($Furfural<$resultados['FurfuralMin'] || $Furfural>$resultados['FurfuralMax']) {
-            $cumplimiento=2;
-        }
-        if($Plomo<$resultados['PlomoMin'] || $Plomo>$resultados['PlomoMax']) {
-            $cumplimiento=2;
-        }
-        if($Cobre<$resultados['ArsenicoMin'] || $Cobre>$resultados['ArsenicoMax']) {
-            $cumplimiento=2;
-        }
+    $cumplimiento = 1;
+    if ($Alcohol < $resultados['PorcentajeAlcoholMin'] || $Alcohol > $resultados['PorcentajeAlcoholMax']) {
+        $cumplimiento = 2;
+    }
+    if ($Metanol < $resultados['MetanolMin'] || $Metanol > $resultados['MetanolMax']) {
+        $cumplimiento = 2;
+    }
+    if ($Superiores < $resultados['AlcoholesSuperioresMin'] || $Superiores > $resultados['AlcoholesSuperioresMax']) {
+        $cumplimiento = 2;
+    }
 
-        $referencia = 1;
-        $q1 = "INSERT INTO analisisdestilado (Clave, Lote, PorcentajeAlcohol, Metanol, AlcoholesSuperiores, Referencia, Cumplimiento)
-                VALUES(:Clave, :Lote, :Alcohol, :Metanol, :AlcoholesSuperiores, :referencia, :cumplimiento)";
-        $a1 = [
-        ":Clave" => $Clave, 
-        ":Lote" => $Lote, 
-        ":Alcohol" => $Alcohol,  
-        ":Metanol" => $Metanol, 
-        ":AlcoholesSuperiores" => $Superiores, 
-        ":referencia" => $referencia, 
-        ":cumplimiento" => $cumplimiento];
-        $querry = $q1;
-        $parametros = $a1;
+    $referencia = 1;
+    $q1 = "UPDATE analisisdestilado SET Lote=:Lote, PorcentajeAlcohol=:Alcohol, Metanol=:Metanol, AlcoholesSuperiores=:AlcoholesSuperiores, Referencia=:Referencia, cumplimiento=:cumplimiento
+            WHERE Clave=:Clave";
+    $a1 = [
+        ":Clave" => $Clave,
+        ":Lote" => $Lote,
+        ":Alcohol" => $Alcohol,
+        ":Metanol" => $Metanol,
+        ":AlcoholesSuperiores" => $Superiores,
+        ":Referencia" => $referencia,
+        ":cumplimiento" => $cumplimiento
+    ];
+    $querry = $q1;
+    $parametros = $a1;
 
-        $this->base->insertar_eliminar_actualizar($querry, $parametros);
-        $this->base->cerrar_conexion();
-        echo json_encode('exito');
+    $this->base->insertar_eliminar_actualizar($querry, $parametros);
+    $this->base->cerrar_conexion();
+    echo json_encode('exito');
     }
 }
